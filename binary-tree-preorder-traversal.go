32bcb9cee01f6754874f4a0ef624b38c6abf61b0
Шапошников Евгений <shaposhnikovea@lad24.ru>
2023-07-21 12:56:10 +0300

    Final binary-tree-preorder-traversal

--- binary-tree-preorder-traversal.go
+++ binary-tree-preorder-traversal.go
@@ -1,10 +1,6 @@
 // https://leetcode.com/problems/binary-tree-preorder-traversal/
 package leetcode
 
-import (
-	. "lc/utils"
-)
-
 // Definition for a binary tree node.
 type TreeNode struct {
 	Val   int
@@ -12,27 +8,30 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func preorder(node *TreeNode, result []int) []int {
+type Tree struct {
+	Root *TreeNode
+	Sum []int
+}
+
+func NewTree(root *TreeNode) Tree {
+	return Tree{ Root: root }
+}
+
+func (tree *Tree) preorder(node *TreeNode) {
 	if node != nil {
-		result = AppendUnique(result, []int{node.Val})
-		result = AppendUnique(result, preorder(node.Left, result))
-		result = AppendUnique(result, preorder(node.Right, result))
-		return result
-	} else {
-		return []int{}
+		tree.Sum = append(tree.Sum, node.Val)
+		tree.preorder(node.Left)
+		tree.preorder(node.Right)
 	}
 }
 
-func preorderTraversal(root *TreeNode) []int {
-	return preorder(root, []int{})
+func (tree *Tree) Preorder() {
+	tree.preorder(tree.Root)
 }
 
-// Input
-// root =
-// [37,-34,-48,null,-100,-100,48,null,null,null,null,-54,null,-71,-22,null,null,null,8]
+func preorderTraversal(root *TreeNode) []int {
+	tree := NewTree(root)
+	tree.Preorder()
+	return tree.Sum
+}
 
-// Use Testcase
-// Output
-// [37,-34,-100,-48,48,-54,-71,-22,8]
-// Expected
-// [37,-34,-100,-48,-100,48,-54,-71,-22,8]
